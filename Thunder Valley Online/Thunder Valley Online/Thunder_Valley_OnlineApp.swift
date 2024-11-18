eb2c4004c971bc4e27924c931b476cf9cd93c190
Dias <dias.atudinov@gmail.com>
2024-11-18 18:23:35 +0500

    start

---
+++ Thunder Valley Online/Thunder Valley Online/Thunder_Valley_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thunder_Valley_OnlineApp.swift
+//  Thunder Valley Online
+//
+//  Created by Dias Atudinov on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Thunder_Valley_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
