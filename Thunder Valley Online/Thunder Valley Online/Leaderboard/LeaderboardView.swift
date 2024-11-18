edba473ba4b7a7e34e523c1cde346ad8f1ba5e12
Dias <dias.atudinov@gmail.com>
2024-11-18 21:17:52 +0500

    Leaderboard view draw

--- Thunder Valley Online/Thunder Valley Online/Leaderboard/LeaderboardView.swift
+++ Thunder Valley Online/Thunder Valley Online/Leaderboard/LeaderboardView.swift
@@ -8,8 +8,100 @@
 import SwiftUI
 
 struct LeaderboardView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var viewModel = LeaderboardViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            ZStack {
+                
+                
+                VStack(spacing: 0) {
+                    Text("Leaderboard")
+                        
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                        .padding(.top)
+                        .padding(.bottom, 15)
+                   
+                    HStack {
+                        Text("Name")
+                        Spacer()
+                        Text("Score")
+                            
+                    }.padding(.horizontal, 20)
+                    .font(.system(size: 16))
+                    .foregroundColor(.white)
+                    .frame(width: 270)
+                    .padding(.bottom, 10)
+                    
+                    VStack(spacing: 10) {
+                        ForEach(viewModel.otherPlayers.indices, id: \.self) { index in
+                            ZStack {
+                               
+                                Image(.leaderboard)
+                                    
+                                HStack(spacing: 0) {
+                                    Text("\(index + 1) - ")
+                                    Text("\(viewModel.otherPlayers[index].name)")
+                                    Spacer()
+                                    Text("\(viewModel.otherPlayers[index].score)")
+                                }.padding(.horizontal, 20)
+                            }.frame(width: 270)
+                        }
+                        
+                        ZStack {
+                           
+                            Image(.leaderboard)
+                                
+                            HStack(spacing: 0) {
+                                Text("55 - ")
+                                Text("YOU")
+                                Spacer()
+                                Text("550")
+                            }.padding(.horizontal, 20)
+                        }.frame(width: 270)
+                    }.font(.system(size: 13))
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                        .padding(.bottom)
+                    
+                    
+
+                    
+                }.background(
+                    
+                    BackgroundMenu()
+                    //.frame(width: 260, height: 236)
+                )
+            }.textCase(.uppercase)
+            
+            VStack {
+                HStack {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(.backBtn)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                    }
+                    
+                    
+                    Spacer()
+                }
+                .padding()
+                Spacer()
+            }
+            
+        }.background(
+            Image(.background)
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+        )
     }
 }
 
--- Thunder Valley Online/Thunder Valley Online/Leaderboard/LeaderboardViewModel.swift
+++ Thunder Valley Online/Thunder Valley Online/Leaderboard/LeaderboardViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class LeaderboardViewModel: ObservableObject {
+    let otherPlayers: [Player] = [
+        Player(name: "Anna", score: 1500),
+        Player(name: "John", score: 1400),
+        Player(name: "Jane", score: 1300),
+        Player(name: "Sam", score: 1250),
+        Player(name: "Helena", score: 1200)
+
+    ]
+}
+
+struct Player: Hashable {
+    let id = UUID()
+    var name: String
+    var score: Int
+}
