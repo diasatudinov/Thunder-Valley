85de4ee38b9c6074c79766984d9cfdcd74aae8d6
Dias <dias.atudinov@gmail.com>
2024-11-19 19:13:18 +0500

    add files

---
+++ Thunder Valley Online/Thunder Valley Online/Menu/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Thunder Valley Online
+//
+//  Created by Dias Atudinov on 19.11.2024.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
---
+++ Thunder Valley Online/Thunder Valley Online/UIElements/PauseShield.swift
@@ -0,0 +1,18 @@
+//
+//  PauseShield.swift
+//  Thunder Valley Online
+//
+//  Created by Dias Atudinov on 19.11.2024.
+//
+
+import SwiftUI
+
+struct PauseShield: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PauseShield()
+}
